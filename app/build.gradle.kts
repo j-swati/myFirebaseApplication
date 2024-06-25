2c97576c59d9796a7815b457830fc6687d7c9345
swati-153 <swati.jidaga@gmail.com>
2024-06-25 13:11:12 +0530

    commented sth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ dependencies {
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    //debugImplementation(libs.androidx.ui.test.manifest)
 
     // Firebase BoM
     implementation(libs.firebase.bom)
